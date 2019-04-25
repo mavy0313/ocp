8f074c4c39c5385d6b4cbdb28bccc85ad33a2da0
mavy0313 <vypoff@gmail.com>
2019-04-25 07:06:10 +0400

    FileReader, FileWriter

---
+++ src/main/java/chapter8/CopyTextFileSample.java
@@ -0,0 +1,36 @@
+package chapter8;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyTextFileSample {
+    public static List<String> readFile(File source) throws IOException {
+        List<String> data = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            String s;
+            while ((s = reader.readLine()) != null) {
+                data.add(s);
+            }
+        }
+        return data;
+    }
+
+    public static void writeFile(List<String> data, File destination) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(destination))) {
+            for (String s: data) {
+                writer.write(s);
+                writer.newLine();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        File source = new File("zoo2.txt");
+        File destination = new File("zoo2Copy.txt");
+        List<String> data = readFile(source);
+        for (String record: data) {
+            System.out.println(record);
+        }
+        writeFile(data, destination);
+    }
+}
