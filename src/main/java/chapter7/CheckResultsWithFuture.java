c021d29bf616fa47c3658dcd94ab7a40619db839
mavy0313 <vypoff@gmail.com>
2019-05-20 07:21:12 +0400

    Future

---
+++ src/main/java/chapter7/CheckResultsWithFuture.java
@@ -0,0 +1,23 @@
+package chapter7;
+
+import java.util.concurrent.*;
+
+public class CheckResultsWithFuture {
+    private static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+            Future<?> result = service.submit(() -> {
+                for(int i=0; i<500; i++) CheckResultsWithFuture.counter++;
+            });
+            System.out.println(result.get(10, TimeUnit.SECONDS)); //null
+            System.out.println("Reached!");
+        } catch (TimeoutException e) {
+            System.out.println("Not reached in time");
+        } finally {
+            if(service != null) service.shutdown();
+        }
+    }
+}
