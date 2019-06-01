d70c5053c6fea5a51d4835ee04893d0dd783fa6a
mavy0313 <vypoff@gmail.com>
2019-06-01 17:28:00 +0400

    CopyOnWrite Synchronized Collections

---
+++ src/main/java/chapter7/CopyOnWriteSample.java
@@ -0,0 +1,19 @@
+package chapter7;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteSample {
+    public static void main(String[] args) {
+        List<Integer> list = new CopyOnWriteArrayList<>(Arrays.asList(4,3,52));
+        for(Integer item: list) {
+            System.out.print(item+" ");
+            list.add(9);
+        }
+        System.out.println();
+        System.out.println("Size: "+list.size());
+        //4 3 52
+        //Size: 6
+    }
+}
